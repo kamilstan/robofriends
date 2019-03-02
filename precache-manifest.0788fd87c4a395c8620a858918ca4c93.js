3275a7801304f3402b4a48b90fbebc0641066c62
Kamil Stangret <kamilstangret@wp.pl>
2019-03-02 13:56:51 +0100

    Updates

---
+++ precache-manifest.0788fd87c4a395c8620a858918ca4c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "6e826980196c6e4b9ec7",
+    "url": "/robofriends/static/js/main.6e826980.chunk.js"
+  },
+  {
+    "revision": "056a46b76a1ca4a495af",
+    "url": "/robofriends/static/js/1.056a46b7.chunk.js"
+  },
+  {
+    "revision": "6e826980196c6e4b9ec7",
+    "url": "/robofriends/static/css/main.1dcec3c6.chunk.css"
+  },
+  {
+    "revision": "056a46b76a1ca4a495af",
+    "url": "/robofriends/static/css/1.2de74b49.chunk.css"
+  },
+  {
+    "revision": "dfddf5edb057c10a5f4c7b9d169b6d73",
+    "url": "/robofriends/index.html"
+  }
+];
